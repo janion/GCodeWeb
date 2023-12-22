7e5954d9d8cb4a4533a9cfb0d2e8196a0ff306ae
Janion <no@nope.nah>
2023-12-22 12:15:09 +0000

    Added 'Download All' button for generated gcode

--- src/gcode_web/output/gcode_output_panel.py
+++ src/gcode_web/output/gcode_output_panel.py
@@ -1,3 +1,6 @@
+import io
+import zipfile
+
 from shiny import Inputs, Outputs, Session, ui, module, render, reactive
 
 from gcode_web.output.gcode_file_output_panel import gcode_file_output_panel_ui, gcode_file_output_panel_server
@@ -21,11 +24,14 @@ def gcode_output_panel_server(input: Inputs, output: Outputs, session: Session, 
                 style='text-align: center'
             )
         else:
-            return ui.navset_tab(
-                *[ui.nav(
-                    file.name,
-                    gcode_file_output_panel_ui(id=f'output_{file.title}')
-                ) for file in files]
+            return ui.div(
+                ui.download_button(id='download_all_btn', label="Download All"),
+                ui.navset_tab(
+                    *[ui.nav(
+                        file.name,
+                        gcode_file_output_panel_ui(id=f'output_{file.title}')
+                    ) for file in files]
+                )
             )
 
     @reactive.Effect
@@ -33,3 +39,14 @@ def gcode_output_panel_server(input: Inputs, output: Outputs, session: Session, 
     def _install_servers():
         for file in generated_files.get():
             gcode_file_output_panel_server(id=f'output_{file.title}', generated_file=file)
+
+        if len(generated_files.get()) > 0:
+            @session.download(filename="all_jobs.zip")
+            async def download_all_btn():
+                zip_buffer = io.BytesIO()
+
+                with zipfile.ZipFile(zip_buffer, "a", zipfile.ZIP_DEFLATED, False) as zip_file:
+                    for file in generated_files.get():
+                        zip_file.writestr(file.name, '\n'.join(file.lines))
+
+                yield zip_buffer.getvalue()
